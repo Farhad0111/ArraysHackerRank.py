7360f9afd0da7063b702745a45faf49234140d02
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-18 22:13:07 +0600

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,21 @@
+'''import numpy
+
+def arrays(arr):
+    return (numpy.array(arr[::-1],float))
+    # complete this function
+    # use numpy.array
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)'''
+
+import numpy
+
+def arrays(arr):
+    return numpy.array(arr[::-1],float)
+    # complete this function
+    # use numpy.array
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
