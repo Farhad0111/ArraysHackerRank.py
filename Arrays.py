2acbec49a6cd5bc60ed877957eb2c67b0df507a3
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-18 22:13:36 +0600

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -1,14 +1,3 @@
-'''import numpy
-
-def arrays(arr):
-    return (numpy.array(arr[::-1],float))
-    # complete this function
-    # use numpy.array
-
-arr = input().strip().split(' ')
-result = arrays(arr)
-print(result)'''
-
 import numpy
 
 def arrays(arr):
